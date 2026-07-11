7da19cf87b5512467c16cfc062877f805436df93
AssBoom <30797596@qq.com>
2026-07-11 10:53:56 +0800

    feat(tooltip): register v-tooltip directive globally

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.css'
 import './assets/theme-variables.css'
+import { vTooltip } from './directives/tooltip'
 
 const app = createApp(App)
 app.use(createPinia())
+app.directive('tooltip', vTooltip)
 app.mount('#app')
